5c3c27e2366aba74cb660459d23a7d86fa56c6ed
MoyangFlower <2013liberalism@sina.com>
2019-09-22 14:16:02 +0800

    增加自定义站点

--- Blog_sys/blog/admin.py
+++ Blog_sys/blog/admin.py
@@ -4,9 +4,10 @@ from django.utils.html import format_html
 from .models import Post, Category, Tag
 
 from .adminforms import PostAdminForm
+from Blog_sys.custom_site import custom_site
 
 
-@admin.register(Category)
+@admin.register(Category, site=custom_site)
 class CategoryAdmin(admin.ModelAdmin):
 	list_display = ('name', 'status', 'is_nav', 'create_time', 'post_count')
 	fields = ('name', 'status', 'is_nav')
@@ -21,7 +22,7 @@ class CategoryAdmin(admin.ModelAdmin):
 		return super(CategoryAdmin, self).save_model(request, obj, form, change)
 
 
-@admin.register(Tag)
+@admin.register(Tag, site=custom_site)
 class TagAdmin(admin.ModelAdmin):
 	list_display = ('name', 'status', 'create_time')
 	fields = ('name', 'status')
@@ -46,7 +47,7 @@ class CategoryOwnerFilter(admin.SimpleListFilter):
 		return queryset
 
 
-@admin.register(Post)
+@admin.register(Post, site=custom_site)
 class PostAdmin(admin.ModelAdmin):
 	form = PostAdminForm
 	list_display = ['title', 'category', 'status', 'create_time', 'owner', 'operator']
@@ -93,7 +94,7 @@ class PostAdmin(admin.ModelAdmin):
 	def operator(self, obj):
 		return format_html(
 			'<a href="{}">编辑</a>',
-			reverse('admin:blog_post_change', args=(obj.id,))
+			reverse('cus_admin:blog_post_change', args=(obj.id,))
 		)
 
 	operator.short_description = '操作'
