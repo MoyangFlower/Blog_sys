e5abbaa66470d81c888a9b70d10fe6242eeb842d
MoyangFlower <2013liberalism@sina.com>
2019-09-22 15:38:32 +0800

    配置模版,开发用户界面

--- Blog_sys/Blog_sys/settings/base.py
+++ Blog_sys/Blog_sys/settings/base.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Blog_sys',
     'blog',
     'config',
     'comment',
--- Blog_sys/blog/views.py
+++ Blog_sys/blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post, Tag
+
+
+def post_list(request, category_id=None, tag_id=None):
+	if tag_id:
+		try:
+			tag = Tag.objects.get(id=tag_id)
+		except Tag.DoesNotExists:
+			post = []
+		else:
+			post = tag.post_set.filter(status=Post.STATUS_NORMAL)
+	else:
+		post = Post.objects.filter(status=Post.STATUS_NORMAL)
+		if category_id:
+			post = post.filter(category_id=category_id)
+	return render(request, 'blog/list.html', context={'post_list': post})
+
+
+def post_detail(request, post_id=None):
+	try:
+		post = Post.objects.get(id=post_id)
+	except Post.DoesNotExist:
+		post = None
+	return render(request, 'blog/detail.html', context={'post': post})
