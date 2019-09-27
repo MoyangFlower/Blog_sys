668e041e599717749c97cf82cf6517663c69359b
MoyangFlower <2013liberalism@sina.com>
2019-09-27 10:50:26 +0800

    增加友链页面

--- Blog_sys/Blog_sys/urls.py
+++ Blog_sys/Blog_sys/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
 from .custom_site import custom_site
-from config.views import links
+from config.views import LinkListView
 from blog.views import (
     PostDetailView, IndexView, CategoryView, TagView, SearchView, AuthorView)
 
@@ -26,9 +26,9 @@ urlpatterns = [
     url(r'^category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category-list'),
     url(r'^tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag-list'),
     url(r'^post/(?P<post_id>\d+)', PostDetailView.as_view(), name='post-detail'),
-    url(r'^links/$', links, name='links'),
     url(r'^super_admin/', admin.site.urls, name='super-admin'),
     url(r'^admin/', custom_site.urls, name='admin'),
     url(r'^search/$', SearchView.as_view(), name='search'),
     url(r'^author/(?P<owner_id>\d+)/$', AuthorView.as_view(), name='author'),
+    url(r'^links/$', LinkListView.as_view(), name='links'),
 ]
